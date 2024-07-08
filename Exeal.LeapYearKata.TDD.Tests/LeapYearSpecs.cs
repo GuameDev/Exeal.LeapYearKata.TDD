b59741f6acee277ee3f31533e9e8057decf83195
Pablo Muñoz <pablommartinezdev@gmail.com>
2024-07-08 12:10:10 +0200

    feat: green->red. make another test to triangulate

--- Exeal.LeapYearKata.TDD.Tests/LeapYearSpecs.cs
+++ Exeal.LeapYearKata.TDD.Tests/LeapYearSpecs.cs
@@ -8,5 +8,12 @@ namespace Exeal.LeapYearKata.TDD.Tests
             var year = new Year() { Value = 2024 };
             Assert.True(year.IsLeap());
         }
+
+        [Fact]
+        public void IsNotLeapIfIsDivisibleBy100ButNoBy400()
+        {
+            var year = new Year() { Value = 1900 };
+            Assert.True(year.IsLeap());
+        }
     }
 }
